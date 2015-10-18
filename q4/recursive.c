50c5aa7c10b578122f5b4ba3ffc9bbb4c72de8b6
ssh <magan0117@hotmail.com>
2015-10-19 01:53:15 +0800

    q4 recursive ver 3
    Astyle change...

--- q4/recursive.c
+++ q4/recursive.c
@@ -5,39 +5,41 @@
 #include <math.h>
 
 
-int FindMaxSumSubArray(int array[], int low, int high){
- 	int i;
-int sum = 0;
-int leftMax = 0;
-int rightMax = 0;
-     /* No element in the array */
-     if (low > high)  
+int FindMaxSumSubArray(int array[], int low, int high)
+{
+    int i;
+    int sum = 0;
+    int leftMax = 0;
+    int rightMax = 0;
+    /* No element in the array */
+    if (low > high)
         return 0;
-     /* One element in the array */
-     if (low == high) 
+    /* One element in the array */
+    if (low == high)
         return fmax(0, array[low]);
-        
-     /* Middle element of the array */   
-     int middle = (low + high) / 2;
-    
-     /* find maximum sum crossing to left */
-     leftMax = sum = 0;
-     for (i = middle; i >= low; i--) {
+
+    /* Middle element of the array */
+    int middle = (low + high) / 2;
+
+    /* find maximum sum crossing to left */
+    leftMax = sum = 0;
+    for (i = middle; i >= low; i--) {
         sum += array[i];
         if (sum > leftMax)
             leftMax = sum;
-     }
-    
-     /* find maximum sum crossing to right */
-     rightMax = sum = 0;
-     for (i = middle+1; i <= high; i++) {
+    }
+
+    /* find maximum sum crossing to right */
+    rightMax = sum = 0;
+    for (i = middle+1; i <= high; i++) {
         sum += array[i];
         if (sum > rightMax)
             rightMax = sum;
-     }
-     
-     /* Return the maximum of leftMax, rightMax and their sum */
-     return fmax(leftMax + rightMax, fmax(FindMaxSumSubArray(array, low, middle), FindMaxSumSubArray(array, middle+1, high)));}
+    }
+
+    /* Return the maximum of leftMax, rightMax and their sum */
+    return fmax(leftMax + rightMax, fmax(FindMaxSumSubArray(array, low, middle), FindMaxSumSubArray(array, middle+1, high)));
+}
 
 
 
