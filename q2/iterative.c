93640f875a01a3e7da9d403b3b538176992f1927
ssh <magan0117@hotmail.com>
2015-10-21 20:46:14 +0800

    q2 iterative ver 2
    astyle

--- q2/iterative.c
+++ q2/iterative.c
@@ -4,20 +4,18 @@
 #include <string.h>
 #include <time.h>
 
-void bubble_sort(char number[],int n){
-char a = '\0';
-         for (int i = 0; i < n; ++i)
-        {
-            for (int j = i + 1; j < n; ++j)
-            {
-                if (number[i] > number[j])
-                {
-                    a =  number[i];
-                    number[i] = number[j];
-                    number[j] = a;
-                }
+void bubble_sort(char number[],int n)
+{
+    char a = '\0';
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (number[i] > number[j]) {
+                a =  number[i];
+                number[i] = number[j];
+                number[j] = a;
             }
         }
+    }
 
 }
 
@@ -38,12 +36,12 @@ void make_string(char test_data[])
     for(int i = 0; i<data_num+1; i++) {
         //small char's ASCII is from 97 - 122
         data_unit = (rand()%26+97) ;
-	
-	test_data[i]=(char)data_unit;
+
+        test_data[i]=(char)data_unit;
     }
-	bubble_sort(test_data,data_num+1);
- for(int i = 0; i<data_num+1; i++)	
-printf("%c,",test_data[i]);
+    bubble_sort(test_data,data_num+1);
+    for(int i = 0; i<data_num+1; i++)
+        printf("%c,",test_data[i]);
 
 
     printf("\n");
@@ -73,7 +71,7 @@ int main()
     char test[] = "\0";
     char find = 'a';
     char ans = '\0';
-	make_string(test);
+    make_string(test);
 
     ans = smallest_character(test,find);
     printf("ANS: %c\n",ans);
