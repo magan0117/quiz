78236b4f74140e68e0fcf548d4cd09a936605a28
shihcc <magan0117@hotmail.com>
2015-10-28 16:06:32 +0800

    add time duration
    as title ,use q2 time duration

--- q4/recursive.c
+++ q4/recursive.c
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
 
 int FindMaxSumSubArray(int array[], int low, int high)
 {
@@ -43,6 +56,8 @@ int FindMaxSumSubArray(int array[], int low, int high)
 
 void TestData()
 {
+    struct timespec start, end;
+    double cpu_time1;
     int *array;
     int m = rand()%20+1;
     //len = m;
@@ -52,7 +67,11 @@ void TestData()
         array[i]=rand()%100+1;
         printf("%d,",array[i]);
     }
+    clock_gettime(CLOCK_REALTIME, &start);
     printf("Ans: %d\n",FindMaxSumSubArray(array,0,m-1));
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time1 = diff_in_second(start, end);
+    printf("execution time of find max sub arr() : %.9lf sec\n", cpu_time1);
 }
 
 
