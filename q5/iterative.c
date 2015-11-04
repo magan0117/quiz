d3352df07346f767c8b82d8b324a864f81b9dc58
ssh <magan0117@hotmail.com>
2015-11-05 01:26:49 +0800

    add Loop Detect, with only one test data
    test data: 1->2->3->4->5->6----->2

--- q5/iterative.c
+++ q5/iterative.c
@@ -22,7 +22,40 @@ void push(struct node** head, int data)
 }
 
 
+
+int DetectLoop(struct node* list)
+{
+    struct node *slow_p = list;
+    struct node *fast_p = list;
+
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if(slow_p == fast_p) {
+            printf("loop!");
+            return fast_p->data;
+        }
+    }
+
+    return 0;
+}
+
+
 int main()
 {
+    struct node* head = NULL;
+
+    push(&head,1);
+    push(&head,2);
+    push(&head,3);
+    push(&head,4);
+    push(&head,5);
+    push(&head,6);
+    //1->2->3->4->5->6------>2  and loop
+    head->next->next->next->next->next = head->next;
+
+    int ans =DetectLoop(head);
+    printf("%d\n",ans);
     return 0;
+
 }
