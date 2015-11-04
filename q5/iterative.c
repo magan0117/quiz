166fb2ab5e51eabfa872ad2ebf41f655f6f8ba00
ssh <magan0117@hotmail.com>
2015-11-05 00:59:37 +0800

    add structure and push function
    as title

--- q5/iterative.c
+++ q5/iterative.c
@@ -1,4 +1,26 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+//type define
+typedef struct node {
+    int data;
+    struct node* next;
+};
+
+//push data
+void push(struct node** head, int data)
+{
+//allocate
+    struct node* new = (struct node*) malloc(sizeof(struct node));
+//put data
+    new->data = data;
+//orig to next
+    new->next = (*head);
+//new one to head
+    (*head) = new;
+
+}
+
 
 int main()
 {
